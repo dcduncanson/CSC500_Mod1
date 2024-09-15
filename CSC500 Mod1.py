5f48fbe21647fc6865a82daf1968f37e65bc7f5a
dcduncanson <dcduncanson@gmail.com>
2024-09-15 16:41:12 -0600

    Add files via upload

---
+++ CSC500 Mod1.py
@@ -0,0 +1,19 @@
+# Part 1
+
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+
+num_sum = int(num1) + int(num2)
+num_difference = int(num1) - int(num2)
+
+print(f'{num1} and {num2} added together are {num_sum} and the difference between the two is {num_difference}.')
+
+# Part 2
+
+num3 = input('Enter the first number: ')
+num4 = input('Enter the second number: ')
+
+num_multiplied = int(num3) * int(num4)
+num_divided = int(num3) / int(num4)
+
+print(f'{num3} and {num4} multiplied together are {num_multiplied} and the two of them divided is {num_divided}.')
